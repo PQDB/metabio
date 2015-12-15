566a9fdc876116f87c17bc3440aeaa9744685c59
Guillaume Larocque <glaroc@gmail.com>
2015-12-15 09:43:43 -0500

    Added files and citations in display template

--- templates/metabio_citations.tpl.php
+++ templates/metabio_citations.tpl.php
@@ -1,3 +1,6 @@
 <div class="metabio-section-header">
   <h2><?php print t('Citations'); ?></h2>
 </div>
+  <?php
+  	echo nl2br($content['publications']);
+  ?>
--- templates/metabio_files.tpl.php
+++ templates/metabio_files.tpl.php
@@ -1,3 +1,14 @@
 <div class="metabio-section-header">
-  <h2><?php print t('Data'); ?></h2>
+<h2><?php echo t('Data'); ?>
+</h2>
 </div>
+  <?php 
+ 		echo '<h2>'.t('Data').'</h2></div>';
+  		echo '<ul class="files_list">';
+  		$files=explode(';',$content['files']);
+  		foreach($files as $f){
+  			$file=file_load($f);
+  			echo '<li><a href="'.file_create_url($file->uri).'">'.$file->filename.'</a></li>';
+  		}
+  		echo '</ul>';
+  ?>  
